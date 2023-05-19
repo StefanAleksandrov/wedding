8ae32eec5ee90a53d595ae9a054df5f7c4e8d8fa
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2023-05-19 14:34:17 +0300

    UPDATE timer logic and add placeholder values

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -2,11 +2,10 @@ const WEDDING_DAY = '08 Jul 2023, 16:00:00';
 
 const weddingDate = new Date(WEDDING_DAY).getTime();
 const containerElement = document.querySelector('#program .timer');
+const today = new Date().getTime();
+let distance = weddingDate - today;
 
 const interval = setInterval(() => {
-    const today = new Date().getTime();
-    const distance = weddingDate - today;
-
     if (distance < 1000) {
         containerElement.innerHTML = `
             <span class="days">00</span>
@@ -25,6 +24,7 @@ const interval = setInterval(() => {
     const hours = Math.floor((distance % (1_000 * 60 * 60 * 24)) / (1_000 * 60 * 60)).toString().padStart(2, '0');
     const minutes = Math.floor((distance % (1_000 * 60 * 60)) / (1_000 * 60)).toString().padStart(2, '0');
     const seconds = Math.floor((distance % (1_000 * 60)) / 1_000).toString().padStart(2, '0');
+    distance -= 1000;
 
     containerElement.innerHTML = `
         <span class="days">${days}</span>

198aef1dc4f00f00a8dc15918c3aaa93071e4734
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2023-05-19 14:21:13 +0300

    ADD countdown timer and style it

---
+++ public/scripts/timer.js
@@ -0,0 +1,38 @@
+const WEDDING_DAY = '08 Jul 2023, 16:00:00';
+
+const weddingDate = new Date(WEDDING_DAY).getTime();
+const containerElement = document.querySelector('#program .timer');
+
+const interval = setInterval(() => {
+    const today = new Date().getTime();
+    const distance = weddingDate - today;
+
+    if (distance < 1000) {
+        containerElement.innerHTML = `
+            <span class="days">00</span>
+            :
+            <span class="hours">00</span>
+            :
+            <span class="minutes">00</span>
+            :
+            <span class="seconds">00</span>
+        `;
+        clearInterval(interval);
+        return;
+    }
+
+    const days = Math.floor(distance / (1_000 * 60 * 60 * 24)).toString().padStart(2, '0');
+    const hours = Math.floor((distance % (1_000 * 60 * 60 * 24)) / (1_000 * 60 * 60)).toString().padStart(2, '0');
+    const minutes = Math.floor((distance % (1_000 * 60 * 60)) / (1_000 * 60)).toString().padStart(2, '0');
+    const seconds = Math.floor((distance % (1_000 * 60)) / 1_000).toString().padStart(2, '0');
+
+    containerElement.innerHTML = `
+        <span class="days">${days}</span>
+        :
+        <span class="hours">${hours}</span>
+        :
+        <span class="minutes">${minutes}</span>
+        :
+        <span class="seconds">${seconds}</span>
+    `;
+}, 1000);
